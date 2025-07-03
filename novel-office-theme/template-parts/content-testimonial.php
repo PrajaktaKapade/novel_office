920b0a3365677486052ac7eef576629f3e79c740
Prajakta Kapade <81553938+PrajaktaKapade@users.noreply.github.com>
2025-07-03 17:03:59 +0530

    Delete novel-office-theme directory

--- novel-office-theme/template-parts/content-testimonial.php
+++
@@ -1,8 +0,0 @@
-<article class="testimonial">
-    <div class="testimonial-content">
-        <?php the_content(); ?>
-    </div>
-    <div class="testimonial-meta">
-        <strong><?php the_title(); ?></strong>
-    </div>
-</article>
