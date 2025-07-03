ff3dbbfc96f85a1035c08540ca9cba420332c869
Prajakta Kapade <81553938+PrajaktaKapade@users.noreply.github.com>
2025-07-03 17:03:15 +0530

    Add files via upload

---
+++ template-parts/content-testimonial.php
@@ -0,0 +1,8 @@
+<article class="testimonial">
+    <div class="testimonial-content">
+        <?php the_content(); ?>
+    </div>
+    <div class="testimonial-meta">
+        <strong><?php the_title(); ?></strong>
+    </div>
+</article>
